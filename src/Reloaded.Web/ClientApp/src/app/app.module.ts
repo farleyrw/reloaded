8371213b20b67a939aaa6e9857922f43b714151f
farleyrw <farleyrw@gmail.com>
2024-06-13 23:35:44 -0400

    update nuget and angular packages

--- src/Reloaded.Web/ClientApp/src/app/app.module.ts
+++ src/Reloaded.Web/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,25 +12,18 @@ import { ResultsModule } from './results/results.module';
 import { ReloadModule } from './reloads/reload.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    FirearmModule,
-    ReloadModule,
-    ResultsModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        FirearmModule,
+        ReloadModule,
+        ResultsModule,
+        AppRoutingModule,
+        NgbModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
 
 /*
